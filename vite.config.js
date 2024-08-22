4da610a7c73ed462547717d1485d7c999fd741b3
lzx <503806316@qq.com>
2024-08-23 03:27:16 +0800

    配置修改

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  publicPath: "./",
-  base: "/vite-vue-01/", //设置项目的根目录
+  publicPath: "/vite-vue-01/",
+  base: "./", //设置项目的根目录
 });
